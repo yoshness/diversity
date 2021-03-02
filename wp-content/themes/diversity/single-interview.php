ea3f1612192e40331332fc929f444d5cab7ac8d8
Josh Dabuet <josiah.dabuet@gmail.com>
2021-03-02 13:03:54 +0800

    Interview Detail static PC done

---
+++ wp-content/themes/diversity/single-interview.php
@@ -0,0 +1,86 @@
+<?php
+/* 
+Template Name: Interview Detail
+*/
+get_header();
+
+?>
+
+<main class="l-single">
+	<div class="l-cover l-cover--detail">
+		<div class="l-cover-bg u-bg-fill" style="background-image: url(<?php echo IMAGE_URL;  ?>detail/cover.jpg);">
+	</div>
+	<div class="l-single__eyecatch u-flex">
+		<div class="l-single__eyecatch-left">
+			<p class="l-single__eyecatch-tagline">個性を引き出し、<br>個性で組織する。<br>そんな会社であり続けたい。</p>
+			<p class="l-single__eyecatch-history">2009年、法政大学法学部卒業後、組織・人材コンサルティングを行うベンチャー企業に入社。人材畑一筋にコンサルティング営業を学んだあと、2012年7月、創業間もないポートに入社。以来、就職支援事業部の中でも採用コンサルティングを中心に行う総合企画グループをけん引する。特技は寿司づくり。魚の目利きから、さばいて握るまで一人でやる。</p>
+		</div>
+		<div class="l-single__eyecatch-right">
+			<h1>
+				<span class="l-single__title-ja">濱田 洋平</span>
+				<span class="l-single__title-en">YOUHEI HAMADA</span>
+			</h1>
+			<p class="l-single__eyecatch-company">大東建託株式会社</p>
+			<p class="l-single__eyecatch-department">ダイバーシティマネジメント　イクボス</p>
+			<p class="l-single__eyecatch-position">業務推進部　部長</p>
+			<h2>＜ 利用制度 ＞</h2>
+			<ul class="l-single__eyecatch-tags u-flex">
+				<li>配偶者出産特別休暇</li>
+				<li>出産祝金</li>
+				<li>育児休業</li>
+			</ul>
+			<p class="l-single__eyecatch-note">※ライフイベントに合わせた福利厚生制度はこちら</p>
+		</div>
+	</div>
+	<div class="l-single__block u-flex">
+		<img src="<?php echo IMAGE_URL;  ?>detail/dummy-1.jpg">
+		<div class="l-single__block-content">
+			<h3>個性を引き出し、個性で組織する。<br>そんな会社であり続けたい。</h3>
+			<p>ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+			<p>ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+		</div>
+	</div>
+	<div class="l-single__block l-single__block--alternate u-flex">
+		<img src="<?php echo IMAGE_URL;  ?>detail/dummy-2.jpg">
+		<div class="l-single__block-content">
+			<h3>家庭と仕事の両立。<br>それこそがあるべき姿。</h3>
+			<p>ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+			<p>ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+		</div>
+	</div>
+	<div class="l-single__block l-single__block--wide">
+		<div class="l-container u-flex">
+			<img src="<?php echo IMAGE_URL;  ?>detail/dummy-3.jpg">
+			<div class="l-single__block-content">
+				<h3>時代と共に変化する、With コロナの働き方</h3>
+				<p>ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+				<p>ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+			</div>
+		</div>
+	</div>
+	<section class="l-single__related">
+		<div class="l-container">
+			<h2>インタビュー一覧</h2>
+			<ul class="l-interview__items u-flex">
+				<?php for($i = 0; $i < 3; $i++) { ?>
+				<li class="interview-block">
+					<a href="#">
+						<img src="<?php echo IMAGE_URL;  ?>interview/dummy.jpg" alt="">
+						<p class="interview-block__tagline">個性を引き出し、個性で組織する。そんな会社であり続けたい。</p>
+						<p class="interview-block__company">大東建託株式会社</p>
+						<p class="interview-block__department">ダイバーシティマネジメント　イクボス</p>
+						<p class="interview-block__position">業務推進部　部長</p>
+						<h3 class="interview-block__name">
+							<span class="interview-block__name-ja">濱田 洋平</span>
+							<span class="interview-block__name-en">YOUHEI HAMADA</span>
+						</h3>
+					</a>
+				</li>
+				<?php } ?>
+			</ul>
+		</div>
+	</section>
+</main>
+
+<?php
+get_footer();
